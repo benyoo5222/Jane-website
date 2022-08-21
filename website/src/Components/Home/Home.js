8db75e07537c6b8206544f6b29b7c52c3b4e7298
Jae Hoon Yoo <jaehoonyoo@Jaes-MacBook-Air.local>
2022-08-21 00:17:58 -0400

    Added photos to the show case section

--- website/src/Components/Home/Home.js
+++ website/src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import "./Home.css";
 import HeroSection from "../Hero-Section/HeroSection";
+import ShowCase from "../Show-Case/ShowCase";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <ShowCase />
     </div>
   );
 };
--- website/src/Components/Show-Case/ShowCase.js
+++ website/src/Components/Show-Case/ShowCase.js
@@ -1,3 +1,62 @@
-const ShowCase = () => {};
+import "./ShowCase.css";
+
+const ShowCase = () => {
+  return (
+    <main className="showcase-container">
+      <h3 className="showcase-title">Jane's Work</h3>
+
+      <div className="showcase-work-container">
+        <section className="showcase-work">
+          <img
+            alt="Person holding a bouquet of flowers"
+            src="/bouquet-of-flowers-original.jpg"
+            className="showcase-work-image"
+          />
+
+          <p className="showcase-work-description">Lorem Ipsum</p>
+        </section>
+
+        <section className="showcase-work">
+          <img
+            alt="A tree with lights"
+            src="/tree-lights.jpg"
+            className="showcase-work-image2"
+          />
+
+          <p className="showcase-work-description2">Lorem Ipsum</p>
+        </section>
+
+        <section className="showcase-work">
+          <img
+            alt="A man and a woman's hands in a wedding"
+            src="/wedding.jpg"
+            className="showcase-work-image"
+          />
+
+          <p className="showcase-work-description">Lorem Ipsum</p>
+        </section>
+
+        <section className="showcase-work">
+          <img
+            alt="Pink flower beside a bible"
+            src="/bible.jpg"
+            className="showcase-work-image2"
+          />
+
+          <p className="showcase-work-description2">Lorem Ipsum</p>
+        </section>
+        <section className="showcase-work">
+          <img
+            alt="Tulip flowers"
+            src="/lily.jpg"
+            className="showcase-work-image"
+          />
+
+          <p className="showcase-work-description">Lorem Ipsum</p>
+        </section>
+      </div>
+    </main>
+  );
+};
 
 export default ShowCase;
