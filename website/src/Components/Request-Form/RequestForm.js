8165bf3612c4539660853df1291f7fc80254483a
Jae Hoon Yoo <jaehoonyoo@Jaes-Mac-mini.local>
2022-09-05 21:17:46 -0300

    Added form validation

--- website/src/Components/Request-Form/RequestForm.js
+++ website/src/Components/Request-Form/RequestForm.js
@@ -1,13 +1,71 @@
 import "./RequestForm.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const RequestForm = () => {
+  const [businessName, setBusinessName] = useState("");
+  const [email, setEmail] = useState("");
+  const [currentDateObject, setCurrentDateObject] = useState(new Date());
+  const [dateSelected, setDateSelected] = useState(
+    `${currentDateObject.getFullYear()}-${`0${
+      currentDateObject.getMonth() + 1
+    }`.slice(-2)}-${`0${currentDateObject.getDate()}`.slice(-2)}`
+  );
+  const [requestDetails, setRequestDetails] = useState("");
   const [radioButtonLiveValue, setRadioButtonLiveValue] = useState(false);
   const [radioButtonOnlineValue, setRadioButtonOnlineValue] = useState(false);
+  const [businessNameFieldWarning, setBusinessNameFieldWarning] =
+    useState(false);
+  const [emailFieldWarning, setEmailFieldWarning] = useState(false);
+  const [dateSelectedFieldWarning, setDateSelectedFieldWarning] =
+    useState(false);
+  const [requestDetailsFieldWarning, setRequestDetailsFieldWarning] =
+    useState(false);
   const [waitingForSubmit, setWaitingForSubmit] = useState(false);
+
+  useEffect(() => {
+    const newDateObject = new Date();
+    setCurrentDateObject(newDateObject);
+    setDateSelected(
+      `${newDateObject.getFullYear()}-${`0${
+        newDateObject.getMonth() + 1
+      }`.slice(-2)}-${`0${newDateObject.getDate()}`.slice(-2)}`
+    );
+  }, []);
+
+  const handleTextChange = (event) => {
+    event.preventDefault();
+    const targetElement = event.target;
+
+    switch (targetElement.name) {
+      case "businessName":
+        if (targetElement.value.length > 0) {
+          setBusinessNameFieldWarning(false);
+        }
+        setBusinessName(targetElement.value);
+        return;
+      case "email":
+        if (targetElement.value.length > 0) {
+          setEmailFieldWarning(false);
+        }
+        setEmail(targetElement.value);
+        return;
+      default:
+        if (targetElement.value.length > 0) {
+          setRequestDetailsFieldWarning(false);
+        }
+        setRequestDetails(targetElement.value);
+        return;
+    }
+  };
+
+  const handleDateChange = (event) => {
+    event.preventDefault();
+    setDateSelectedFieldWarning(false);
+    setDateSelected(event.target.value);
+  };
 
   const handleRadioButtonSelection = (event) => {
     const setter =
@@ -18,13 +76,144 @@ const RequestForm = () => {
     setter((previousValue) => !previousValue);
   };
 
-  const createField = ({ titleOfField, type }, index) => {
+  const isSelectedDateGreaterThanCurrentDate = () => {
+    if (!dateSelected || dateSelected === "Invalid Date") {
+      return false;
+    }
+
+    const currentYear = currentDateObject.getFullYear();
+    const currentMonth = currentDateObject.getMonth() + 1;
+    const currentDate = currentDateObject.getDate();
+
+    const selectedDateString = dateSelected.split("-");
+    return (
+      currentYear <= selectedDateString[0] &&
+      currentMonth <= selectedDateString[1] &&
+      currentDate <= selectedDateString[2]
+    );
+  };
+
+  const validateForm = () => {
+    const isValidEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (
+      businessName &&
+      requestDetails &&
+      dateSelected &&
+      dateSelected !== "Invalid Date" &&
+      isSelectedDateGreaterThanCurrentDate() &&
+      email.length > 0 &&
+      email.match(isValidEmail)
+    ) {
+      setBusinessNameFieldWarning(false);
+      setDateSelectedFieldWarning(false);
+      setRequestDetailsFieldWarning(false);
+      setEmailFieldWarning(false);
+      return true;
+    }
+
+    if (!businessName) {
+      setBusinessNameFieldWarning(true);
+    } else {
+      setBusinessNameFieldWarning(false);
+    }
+
+    if (
+      !dateSelected ||
+      dateSelected === "Invalid Date" ||
+      !isSelectedDateGreaterThanCurrentDate()
+    ) {
+      setDateSelectedFieldWarning(true);
+    } else {
+      setDateSelectedFieldWarning(false);
+    }
+
+    if (!requestDetails) {
+      setRequestDetailsFieldWarning(true);
+    } else {
+      setRequestDetailsFieldWarning(false);
+    }
+
+    if (email.length === 0 || !email.match(isValidEmail)) {
+      setEmailFieldWarning(true);
+    } else {
+      setEmailFieldWarning(false);
+    }
+
+    toast.warning("Please enter valid information in the following fields.");
+  };
+
+  const handleSubmit = async () => {
+    if (waitingForSubmit) {
+      return;
+    }
+
+    setWaitingForSubmit(true);
+
+    if (!validateForm()) {
+      setWaitingForSubmit(false);
+      return;
+    }
+
+    // Send form to API
+    const headers = {
+      "x-api-key": process.env.REACT_APP_API_KEY,
+    };
+
+    try {
+      const sendFormToAPI = await axios.post(
+        `${process.env.REACT_APP_API_URL}/sendemail`,
+        {
+          businessName,
+          email,
+          requestDetails,
+          dateSelected,
+        },
+        { headers: headers }
+      );
+
+      console.log("Result from send form to API", sendFormToAPI);
+
+      setWaitingForSubmit(false);
+      toast.success(
+        "Your request has successfully been submitted! Due to higher than expected demand, it might take a little longer than expected to respond but Jane will get back to your as soon as possible, thank you!"
+      );
+    } catch (error) {
+      console.log("Error sending form to API", error);
+      setWaitingForSubmit(false);
+
+      if (error.response.status === 429) {
+        console.log("Too many request");
+        toast.warn("Please try your request again after a few minutes.");
+        return;
+      }
+
+      toast.error(
+        "Sorry, we are having technical difficulties. Please email test@gmail.com"
+      );
+      return;
+    }
+  };
+
+  const createField = (
+    { titleOfField, type, value, name, warningClass },
+    index
+  ) => {
     switch (type) {
       case "date":
         return (
           <div className="field-container" key={index}>
             <label className="field-label">{titleOfField}</label>
-            <input type="date" />
+            <input
+              className={warningClass ? "required-field" : ""}
+              type="date"
+              value={value}
+              onChange={handleDateChange}
+              name={name}
+              min={`${currentDateObject.getFullYear()}-${`0${
+                currentDateObject.getMonth() + 1
+              }`.slice(-2)}-${`0${currentDateObject.getDate()}`.slice(-2)}`}
+            />
           </div>
         );
       case "liveOrOnline":
@@ -62,58 +251,27 @@ const RequestForm = () => {
         return (
           <div className="field-container" key={index}>
             <label className="field-label">{titleOfField}</label>
-            <textarea className="field-input-textarea" type="text" />
+            <textarea
+              className={`field-input-textarea ${
+                warningClass ? "required-field" : ""
+              }`}
+              type="text"
+              onChange={handleTextChange}
+            />
           </div>
         );
       default:
         return (
           <div className="field-container" key={index}>
             <label className="field-label">{titleOfField}</label>
-            <input className="field-input" type="text" />
+            <input
+              className={`field-input ${warningClass ? "required-field" : ""}`}
+              type="text"
+              onChange={handleTextChange}
+              name={name}
+            />
           </div>
         );
-    }
-  };
-
-  const handleSubmit = async () => {
-    if (waitingForSubmit) {
-      return;
-    }
-
-    setWaitingForSubmit(true);
-
-    // Send form to API
-    const headers = {
-      "x-api-key": process.env.REACT_APP_API_KEY,
-    };
-
-    try {
-      const sendFormToAPI = await axios.post(
-        `${process.env.REACT_APP_API_URL}/sendemail`,
-        { test: "test" },
-        { headers: headers }
-      );
-
-      console.log("Result from send form to API", sendFormToAPI);
-
-      setWaitingForSubmit(false);
-      toast.success(
-        "Your request has successfully been submitted! Due to higher than expected demand, it might take a little longer than expected to respond but Jane will get back to your as soon as possible, thank you!"
-      );
-    } catch (error) {
-      console.log("Error sending form to API", error);
-      setWaitingForSubmit(false);
-
-      if (error.response.status === 429) {
-        console.log("Too many request");
-        toast.warn("Please try your request again after a few minutes.");
-        return;
-      }
-
-      toast.error(
-        "Sorry, we are having technical difficulties. Please email test@gmail.com"
-      );
-      return;
     }
   };
 
@@ -124,12 +282,37 @@ const RequestForm = () => {
       <div className="request-form-section-container">
         <section className="request-form-field-section">
           {[
-            { titleOfField: "Name/ Business Name", type: "text" },
-            { titleOfField: "Email", type: "text" },
-            { titleOfField: "Pick a Date", type: "date" },
+            {
+              titleOfField: "Name/ Business Name*",
+              type: "text",
+              value: businessName,
+              name: "businessName",
+              warningClass: businessNameFieldWarning,
+            },
+            {
+              titleOfField: "Email*",
+              type: "text",
+              value: email,
+              name: "email",
+              warningClass: emailFieldWarning,
+            },
+            {
+              titleOfField: "Pick a Date*",
+              type: "date",
+              value: dateSelected,
+              warningClass: dateSelectedFieldWarning,
+            },
             { titleOfField: "Live/ Online", type: "liveOrOnline" },
-            { titleOfField: "Request Details", type: "textArea" },
-          ].map((title, index) => createField(title, index))}
+            {
+              titleOfField: "Request Details*",
+              type: "textArea",
+              value: requestDetails,
+              name: "requestDetails",
+              warningClass: requestDetailsFieldWarning,
+            },
+          ].map((formInformation, index) =>
+            createField(formInformation, index)
+          )}
 
           <button
             className={`${
